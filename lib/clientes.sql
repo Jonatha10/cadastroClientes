f51a65d4a281b2bf9dcad8a303572bccfde1ec24
Jonatha Santos <71782875+Jonatha10@users.noreply.github.com>
2022-04-19 14:46:34 -0300

    ADICIONADO INSERT'S NO ARQUIVO CLIENTES

--- lib/clientes.sql
+++ lib/clientes.sql
@@ -9,3 +9,7 @@ CREATE TABLE Clientes(
   );
   SELECT * FROM Clientes; 
   
+  
+insert into `Clientes` (`cpf`, `email`, `id`, `nome`, `telefone`) values ('12345689', 'jao123@gmail.com', 1, 'joao', '(11) 1111-4444');
+insert into `Clientes` (`cpf`, `email`, `id`, `nome`, `telefone`) values ('12345689', 'jurema@gmail.com', 2, 'jurema', '(11) 1111-4444');
+insert into `Clientes` (`cpf`, `email`, `id`, `nome`, `telefone`) values ('123456789', 'mario@gmail.com', 3, 'mario', '123456789');
